7880321ed0df0c28ebab1bee9762155519227ca0
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-VKM0MRH>
2021-01-13 11:41:58 -0300

    kit cakes

--- kitcakes.php
+++ kitcakes.php
@@ -5,6 +5,7 @@
         <title>Kit Cakes</title>
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" href="plugins/icomoon/icomoon.css" />
+        <link rel="stylesheet" href="assets/css/header.css" />
         <link rel="stylesheet" href="assets/css/kitcakes.css" /> 
 
     </head>
@@ -15,14 +16,51 @@
 
     <body>
 
+        
         <header class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark vini">
+                <div class="container">
 
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/img/logo-branco.png" width="100" height="100" class="d-inline-block align-top" alt="">
+
+                    </a>
+
+                    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                        <ul class="navbar-nav mr-auto">
+
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0 ">
+                            <ul class="navbar-nav mr-auto"> 
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"><h5>Inicio</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"><h5>Produtos</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"><h5>Contatos</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-facebook2"></span></h5></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-instagram"></h5></span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-twitter"></h5></span></a>
+                                </li>                                                      
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </nav>
 
         </header>
+        
+        
 
-        <?php
-        /* Aqui será o corpo do site... */
-        ?>
+
 
         <footer>
 
