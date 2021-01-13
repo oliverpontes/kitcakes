816f943d609dc935c782c1acba1ef6dfc7259eea
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-VKM0MRH>
2021-01-13 09:15:15 -0300

    projeto kit cakes

---
+++ kitcakes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Kit Cakes</title>
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="plugins/icomoon/icomoon.css" />
+    <!-- <link rel="stylesheet" href="" /> -->
+
+</head>
+
+<<header>
+    
+</header>
+    
+
+
+<body>
+    
+</body>
+
+
+
+
+<footer>
+    
+</footer>
+
+</html>
