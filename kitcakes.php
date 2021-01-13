ee51c47b325710bfaa29f6343505d2b287ea1212
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-VKM0MRH>
2021-01-13 09:33:06 -0300

    projeto kit cakes

--- kitcakes.php
+++ kitcakes.php
@@ -5,25 +5,37 @@
         <title>Kit Cakes</title>
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" href="plugins/icomoon/icomoon.css" />
-        <!-- <link rel="stylesheet" href="" /> -->
+        <link rel="stylesheet" href="assets/css/kitcakes.css" /> 
 
     </head>
 
-    <header>
 
-    </header>
 
 
 
     <body>
+
+        <header>
+
+
+        </header>
+
+        <-- Aqui será escrito o corpo do site.-->
+
+        <footer>
+
+            <script src="assets/js/jquery-3.5.1.min.js"></script>
+
+
+            <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+
+        </footer>
 
     </body>
 
 
 
 
-    <footer>
 
-    </footer>
 
 </html>
