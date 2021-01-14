cf11c671f0701fd453938d1990e6e0b6044574e1
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-VKM0MRH>
2021-01-14 08:55:34 -0300

    kitcakes devidamente linkado..

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+//Meu código vem aqui.
+
+$pg = isset($_GET['pg']);
+
+if ($pg) {
+    switch ($_GET['pg']) {
+
+        case 'inicio':
+            // Incluir a página inicio
+
+
+            include_once 'paginas/includes/header.php';
+            include_once 'paginas/inicio.php';
+            include_once 'paginas/includes/footer.php';
+            break;
+
+        case 'contatos':
+            // Incluir a página contato
+
+            include_once 'paginas/includes/header.php';
+            include_once 'paginas/contatos.php';
+            include_once 'paginas/includes/footer.php';
+
+            break;
+
+
+
+        case 'produtos':
+            // Incluir a página produtos
+
+            include_once 'paginas/includes/header.php';
+            include_once 'paginas/produtos.php';
+            include_once 'paginas/includes/footer.php';
+            break;
+
+
+        default:
+            // Página de Erro.
+            echo 'Página não encontrada';
+            break;
+    }
+} else {
+    include_once 'paginas/includes/header.php';
+    include_once 'paginas/inicio.php';
+    include_once 'paginas/includes/footer.php';
+}
+?>
---
+++ paginas/includes/footer.php
@@ -0,0 +1,45 @@
+<footer>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col">
+                <ul class="list-group rodape">
+                    <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                    <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                    <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                    <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                </ul>
+            </div>
+            <div class="col py-5">
+                <img class="img-fluid img-thumbnail arredondado"src="https://www.placehold.it/70x70" alt="">
+                <h5>Teste</h5>
+                <small>&copy Todos os direitos reservados</small>
+            </div>
+            <div class="col">
+                <ul class="list-group rodape">
+                    <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                    <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                    <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                    <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+<!--Scripts JS-->
+<!--JQuery-->
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+
+
+
+
+
+
+
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+
+
+
+</body>
+</html>
