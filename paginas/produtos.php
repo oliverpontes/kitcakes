29c3b6cb5602d3ad3559599d4b5bf45e28967c1b
PROGR WEB - MANHÃ <PROGR WEB - MANH@MININT-9SOJ8BE>
2021-01-15 10:30:19 -0200

    Alterações do produto/slide

--- paginas/produtos.php
+++ paginas/produtos.php
@@ -46,31 +46,80 @@
     <div class="row mt-2 mb-2">
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                <img class="card-img-top" src="assets/img/infantil.png" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">Bolo De aniversário Infantil</h5>
+                    <p class="card-text">Detalhes:Bolo infantil temático com pasta americana colorida.
+                        <br>
+                        Código do Produto: 0904389
+                        <br>
+                        Preço:
+                        <br>
+                        1kg: 49.99
+                        <br>
+                        2kg: 89.90
+                        <br>
+                        3kg: 120.00
+                        <br>
+                        obs: Para bolos temáticos deixe sua ideia de decoração na 
+                        página de contato e descreva seu tema, após o envio entraremos em contato
+                        conforme os dados obtidos no cadastro. </p>
+                    <a href="#" class="btn btn-danger">Fazer Pedido</a>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                <img class="card-img-top" src="assets/img/noiva.png" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">Bolo de Noiva</h5>
+                    <p class="card-text">Detalhes: Bolo de noiva dois andares
+                        <br>
+
+                        Código do Produto: 0905890
+                        <br>
+
+                        Preço:
+                        <br>
+
+                        1kg: 80.00
+                        2kg: 125.00
+                        3kg: 175.00
+                        <br>
+
+
+                        obs: Para bolos temáticos deixe sua ideia de decoração na 
+                        página de contato e descreva seu tema, após o envio entraremos em contato
+                        conforme os dados obtidos no cadastro.</p>
+                    <a href="#" class="btn btn-danger">Fazer Pedido</a>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                <img class="card-img-top" src="assets/img/15anos.png" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">Bolo 15 anos</h5>
+                    <p class="card-text">Detalhes: Bolo decorativo para festa de 15 anos
+                        <br>
+
+                        Código do Produto: 1515109
+                        <br>
+
+                        Preço:
+                        <br>
+
+                        Preço: 
+                        1kg: 75.00
+                        2kg: 110.00
+                        3kg: 150.00
+                        <br>
+
+
+                        obs: Para bolos temáticos deixe sua ideia de decoração na 
+                        página de contato e descreva seu tema, após o envio entraremos em contato
+                        conforme os dados obtidos no cadastro.</p>
+                    <a href="#" class="btn btn-danger">Fazer Pedido</a>
                 </div>
             </div>
         </div>
@@ -78,31 +127,105 @@
     <div class="row mt-2 mb-2">
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                <img class="card-img-top" src="assets/img/12 (1).png" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">KitFesta 1</h5>
+                    <p class="card-text">Detalhes: Torta de 1kg (Não temática) +100 doces e +100 salgagos
+                        <br>
+                        Escolha os tipos dos salgados:
+                        <br>
+
+                        Coxinha; Empadinha de Frango; Canudinho de carne moída; Bolinho de Bacalhau; Mini enroladinho; Salgadinho de queijo; Pastelzinho de carne; Pastelzinho de Frango; Empada de Camarão.
+                        <br>
+
+
+                        Doces:
+                        <br>
+
+                        Brigadeiro; Bem-casado; Surpresa de uva; Napolitano; Beijinho; Empadinha doce.
+                        <br>
+
+
+
+                        Preço do kit: 99.99$
+                        <br>
+
+                        Código do produto: 95846505
+                        <br>
+
+                        obs: este kit não dá direito ao bolo temático,na página de contato só serão creditados as escolhas do sabor da massa; recheio e cobertura.
+                        deixe ciente no cadastro os tipos de salgados e doces que serão escolhidos para o kit!</p>
+                    <a href="#" class="btn btn-danger">Fazer Pedido</a>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                <img class="card-img-top" src="assets/img/12 (3).png" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">KitFesta 2</h5>
+                    <p class="card-text">Detalhes: Torta de 2kg (Não temática) +200 doces e +200 salgagos
+
+                        <br>
+                        Escolha os tipos dos salgados:
+                        <br>
+
+                        Coxinha; Empadinha de Frango; Canudinho de carne moída; Bolinho de Bacalhau; Mini enroladinho; Salgadinho de queijo; Pastelzinho de carne; Pastelzinho de Frango; Empada de Camarão.
+                        <br>
+
+
+                        Doces:
+                        <br>
+
+                        Brigadeiro; Bem-casado; Surpresa de uva; Napolitano; Beijinho; Empadinha doce.
+                        <br>
+
+
+
+                        Preço do kit: 189.00$$
+                        <br>
+
+                        Código do produto: 5867640
+                        <br>
+
+                        obs: este kit não dá direito ao bolo temático,na página de contato só serão creditados as escolhas do sabor da massa; recheio e cobertura.
+                        deixe ciente no cadastro os tipos de salgados e doces que serão escolhidos para o kit!</p>
+                    <a href="#" class="btn btn-danger">Fazer Pedido</a>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                <img class="card-img-top" src="assets/img/12 (2).png" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h5 class="card-title">Título do card</h5>
-                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <h5 class="card-title">KitFesta 3</h5>
+                    <p class="card-text">Detalhes: Torta de 3kg (Não temática) +200 doces e +200 salgagos
+
+                        <br>
+                        Escolha os tipos dos salgados:
+                        <br>
+
+                        Coxinha; Empadinha de Frango; Canudinho de carne moída; Bolinho de Bacalhau; Mini enroladinho; Salgadinho de queijo; Pastelzinho de carne; Pastelzinho de Frango; Empada de Camarão.
+                        <br>
+
+
+                        Doces:
+                        <br>
+
+                        Brigadeiro; Bem-casado; Surpresa de uva; Napolitano; Beijinho; Empadinha doce.
+                        <br>
+
+
+
+                        Preço do kit: 240.00$
+                        <br>
+
+                        Código do produto:  6570484
+                        <br>
+
+                        obs: este kit não dá direito ao bolo temático,na página de contato só serão creditados as escolhas do sabor da massa; recheio e cobertura.
+                        deixe ciente no cadastro os tipos de salgados e doces que serão escolhidos para o kit!</p>
+                    <a href="#" class="btn btn-danger">Fazer Pedido</a>
                 </div>
             </div>
         </div>
